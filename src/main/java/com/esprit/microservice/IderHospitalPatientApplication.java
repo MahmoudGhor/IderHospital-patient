7d7a2024b090be3e3cfd74c789ae98769edf2a18
mghorbel <mahmoud.ghorbel@esprit.tn>
2019-11-10 11:19:45 +0000

    first commit

---
+++ src/main/java/com/esprit/microservice/IderHospitalPatientApplication.java
@@ -0,0 +1,13 @@
+package com.esprit.microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IderHospitalPatientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IderHospitalPatientApplication.class, args);
+	}
+
+}
